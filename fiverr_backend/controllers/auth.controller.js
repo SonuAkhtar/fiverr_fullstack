3f45317dcdbd654d0094909b518577da95a3dcb1
SonuAkhtar <sonua981@gmail.com>
2023-07-08 18:33:17 +0530

    jwt, error handling added

--- fiverr_backend/controllers/auth.controller.js
+++ fiverr_backend/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
 
@@ -13,7 +15,7 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("User has been created.");
   } catch (error) {
-    res.status(500).send("error something went wrong!");
+    next(error);
   }
 };
 
@@ -22,16 +24,37 @@ export const login = async (req, res) => {
     const user = await User.findOne({
       username: req.body.username,
     });
-    if (!user) return res.stats(500).send("User not found");
+    const err = new Error();
+    err.status = 404;
+    err.message = "User not found";
+    if (!user) return next(createError(404, "User not found"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    if (!isCorrect) return res.status(400).send("Wrong username or password");
+    if (!isCorrect) return next(createError(400, "Wrong username or password"));
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
     const { password, ...info } = user._doc;
-    res.status(200).send(info);
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
-    res.status(500).send("error something went wrong!");
+    next(createError(500, "Something went wrong!"));
   }
 };
 
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", { sameSite: "none", secure: true })
+    .status(200)
+    .send("User has been logged out");
+};
--- fiverr_backend/server.js
+++ fiverr_backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //routes
 import authRoute from "./routes/auth.route.js";
@@ -26,6 +27,7 @@ const connect = async () => {
 };
 
 app.use(express.json());
+app.use(cookieParser());
 
 // use routes
 app.use("/api/auth", authRoute);
@@ -35,6 +37,13 @@ app.use("/api/auth", authRoute);
 // app.use("/api/order", orderRoute);
 // app.use("/api/reivew", reviewRoute);
 app.use("/api/users", userRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 app.listen(8800, () => {
   connect();

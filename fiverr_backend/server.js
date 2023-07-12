8e825a0706d2abe5ace086cb524acba5f8e302d8
SonuAkhtar <sonua981@gmail.com>
2023-07-12 15:08:23 +0530

    register, login pages updated

--- fiverr_backend/server.js
+++ fiverr_backend/server.js
@@ -3,14 +3,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 //routes
 import authRoute from "./routes/auth.route.js";
-// import conversationRoute from "./routes/conversation.route.js";
-// import gigRoute from "./routes/gig.route.js";
-// import messageRoute from "./routes/message.route.js";
-// import orderRoute from "./routes/order.route.js";
-// import reviewRoute from "./routes/review.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import gigRoute from "./routes/gig.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
 import userRoute from "./routes/user.route.js";
 
 const app = express();
@@ -26,16 +27,17 @@ const connect = async () => {
   }
 };
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 // use routes
 app.use("/api/auth", authRoute);
-// app.use("/api/conversation", conversationRoute);
-// app.use("/api/gig", gigRoute);
-// app.use("/api/message", messageRoute);
-// app.use("/api/order", orderRoute);
-// app.use("/api/reivew", reviewRoute);
+app.use("/api/conversation", conversationRoute);
+app.use("/api/gig", gigRoute);
+app.use("/api/message", messageRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/reivew", reviewRoute);
 app.use("/api/users", userRoute);
 
 app.use((err, req, res, next) => {

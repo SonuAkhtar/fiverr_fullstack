a61116ba23c3a20e90ddfdd0cd857e4bf9af833a
SonuAkhtar <sonua981@gmail.com>
2023-07-20 17:08:22 +0530

    payments and orders updated

--- fiverr_backend/controllers/order.controller.js
+++ fiverr_backend/controllers/order.controller.js
@@ -1,25 +1,50 @@
 import createError from "../utils/createError.js";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
+import Stripe from "stripe";
 
-export const createOrder = async (req, res, next) => {
+export const intent = async (req, res, next) => {
+  const gig = await Gig.findById(req.params.id);
+
+  const stripe = new Stripe(process.env.STRIPE);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    image: gig.coverImage,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
+
+export const confirm = async (req, res, next) => {
   try {
-    const gig = await Gig.findById(req.params.gigId);
+    const orders = await Order.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      {
+        $set: { isCompleted: true },
+      }
+    );
 
-    const newOrder = new Order({
-      gigId: gig._id,
-      image: gig.coverImage,
-      title: gig.title,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      price: gig.price,
-      payment_intent: "temporary",
-    });
-
-    await newOrder.save();
-    res.status(200).send("Successful");
+    res.status(200).send(orders);
   } catch (error) {
-    next(error);
+    console.log(error);
   }
 };
 
